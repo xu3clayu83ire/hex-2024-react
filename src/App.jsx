9f01cc16dcf5338c19465f87e6293b0ade88e593
xu3woli <47500703+amgithub07@users.noreply.github.com>
2025-01-12 16:45:01 +0800

    250112-init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import * as bootstrap from "bootstrap";
+import "./assets/style.css";
+
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
+
+function App() {
+  return <></>;
+}
+
+export default App;
